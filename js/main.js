62113b8a18efa0b3a7f9a80a4ee9bcd1d4d5d734
Ahmed Omar Ali <madkey8@gmail.com>
2019-12-08 13:14:27 +0300

    unit 5 project

--- js/main.js
+++ js/main.js
@@ -13,54 +13,5 @@ $('.searchInput').hideseek({
     nodata: 'No results found'
 });
 
-// const pictures = [];
-// $.each($('.section a'), function (i , a) {
-//  const dataValues = $(a).attr('data-value');
-//  pictures.push(dataValues);
-// });
-//
-// jQuery(".searchInput").on("change keyup click paste", function() {
-//     let input = $(".searchInput")
-//         .val()
-//         .toLowerCase();
-//     if (input !== "") {
-//         $("a")
-//             .parent()
-//             .css("display", "none");
-//         pictures.map(function(title) {
-//             if (title.toLowerCase().includes(input)) {
-//                 $(`a[data-value="${title}"]`)
-//                     .parent()
-//                     .css("display", "block");
-//             }
-//         });
-//     } else {
-//         $("a").show();
-//     }
-// });
 
-// const titles = [];
-// $.each($(".section a"), function(i, a) {
-//     const dataTitle = $(a).attr("data-title");
-//     titles.push(dataTitle);
-// });
-//
-// jQuery(".searchInput").on("change keyup click paste", function() {
-//     let input = $(".searchInput")
-//         .val()
-//         .toLowerCase();
-//     if (input !== "") {
-//         $("a")
-//             .parent()
-//             .css("display", "none");
-//         titles.map(function(title) {
-//             if (title.toLowerCase().includes(input)) {
-//                 $(`a[data-title="${title}"]`)
-//                     .parent()
-//                     .css("display", "block");
-//             }
-//         });
-//     } else {
-//         $("a").show();
-//     }
-//});
+
