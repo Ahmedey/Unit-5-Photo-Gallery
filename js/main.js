7c154753db65e9d9239dd242a7f27a56e38419f7
Ahmed Omar Ali <madkey8@gmail.com>
2019-12-08 18:45:24 +0300

    unit 5 project

--- js/main.js
+++ js/main.js
@@ -8,10 +8,33 @@ lightbox.option({
     'albumLabel': ""
 
 });
-$('.searchInput').hideseek({
-    attribute: 'data-title',
-    nodata: 'No results found'
+const searchImage = [];
+$.each($(".section a"), function(i, a) {
+    const dataTitle = $(a).attr("data-title");
+    searchImage.push(dataTitle);
 });
+
+$(".searchInput").on("keyup", function() {
+    let input = $(".searchInput")
+        .val()
+        .toLowerCase();
+    if (input !== "") {
+        $("a")
+            .parent()
+            .css("display", "none");
+        searchImage.map(function(title) {
+            if (title.toLowerCase().includes(input)) {
+                $(`a[data-title="${title}"]`)
+                    .parent()
+                    .css("display", "block");
+            }
+        });
+    } else {
+        $("a").show();
+    }
+});
+
+
 
 
 
